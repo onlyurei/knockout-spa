3af8e2c52d30a5a14b761c1a2c03d77368e44485
Cheng Fan <cfan@eb.com>
2015-01-19 18:20:53 -0600

    Update persisted-observable.

--- js/util/persisted-observable.js
+++ js/util/persisted-observable.js
@@ -1,8 +1,15 @@
 define(['Storage', 'Knockout.Raw'], function (Storage, ko) {
 
+    function unwrapInitialValueFallback(initialValueFallback) {
+        if (ko.isObservable(initialValueFallback) || (typeof initialValueFallback == 'function')) {
+            return initialValueFallback();
+        }
+        return initialValueFallback;
+    }
+
     return function (storageKey, initialValueFallback, permanent) {
         var value = Storage.get(storageKey, permanent);
-        var observable = ko.observable((value || (typeof value == 'boolean')) ? value : initialValueFallback);
+        var observable = ko.observable((value || (typeof value == 'boolean')) ? value : unwrapInitialValueFallback(initialValueFallback));
         return ko.computed({
             read: function () {
                 return observable();
