39870a9379dcdc7c5194526b3130eb28a343c50a
Cheng Fan <fancheng33@hotmail.com>
2015-01-21 20:23:50 -0600

    Apply ko bindings at html root node; set page title.

--- js/framework/page.js
+++ js/framework/page.js
@@ -2,10 +2,16 @@ define(['RootBindings', 'Knockout', 'Sugar'], function (RootBindings, ko) {
 
     var initialRun = true;
 
+    function setPageTitle() {
+        var title = Page.page().data.title;
+        document.title = title ? (Object.isFunction(title) ? title() : title) : (Page.title ? (Object.isFunction(Page.title) ? Page.title() : Page.title) : '');
+    }
+
     var Page = {
         init: function (name, data, callback) {
             name = name.toLowerCase();
             if ((Page.page().name == name) && (Page.page().data == data)) { // if the requested page is the same page, immediately callback
+                setPageTitle();
                 if (callback) {
                     callback(data);
                 }
@@ -21,8 +27,9 @@ define(['RootBindings', 'Knockout', 'Sugar'], function (RootBindings, ko) {
                 name: name,
                 data: data
             }); // to test if template finished rendering, use afterRender binding
+            setPageTitle();
             if (initialRun) {
-                ko.applyBindings(Page);
+                ko.applyBindings(Page, document.getElementsByTagName('html')[0]);
                 initialRun = false;
             }
             if (callback && (initialized !== false)) {
@@ -37,7 +44,10 @@ define(['RootBindings', 'Knockout', 'Sugar'], function (RootBindings, ko) {
                 dispose: function () {}
             }
         }),
-        bodyClass: ko.observable('')
+        bodyClass: ko.observable(''),
+        title: function () {
+            return Page.page().name.titleize(); // override in RootBindings as needed
+        }
     };
 
     Object.merge(Page, RootBindings); // additional root bindings as needed by the app
