8815ac188636393b9e8017c6f0d13b3ddc396b50
Cheng Fan <cfan@groupon.com>
2016-03-03 16:36:28 -0600

    Update README; change port from 3000 to 8080.

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 var root = __dirname;
 app.use(express.static(root));
 app.use(fallback('index.html', { root: root }));
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Visit http://localhost:' + port + ' to see the app.');
