e3c546ac32f222c11ed03a3baefcef48636677d7
Cheng Fan <cfan@groupon.com>
2016-03-08 12:27:37 -0600

    Improve documentation for api calls.

--- js/app/files/files.js
+++ js/app/files/files.js
@@ -8,6 +8,11 @@ define(['app/shared/api/api', 'ko', 'sugar'], function (Api, ko) {
           return unwantedFilesRegex.test(file);
         }));
       });
+      //Api call with full params example:
+      //         apiPackage, apiMethod, urlParams,     data,           error,        loading,          synchronousOrSocket
+      //Api.call('file',     'example', { id: '123' }, { data: data }, errorHandler, loadingIndicator, true).then(function (data) {
+      //  return data;
+      //}).done(function (data) { console.log(data); });
     },
     controllers: {
       '/': function () {
--- js/app/shared/api/api-file.js
+++ js/app/shared/api/api-file.js
@@ -1,8 +1,14 @@
 define({
   _name: 'file',
-/*  _crud: true,
-  https: true,*/
-  list: {
-    type: 'GET'
-  }
+/* _crud: true, //(true|[false]) have auto-generated default CRUD methods [optional]
+  https: true, //(true|[false]) must call all methods of this api package via HTTPS [optional] */
+  list: {} //api method, if type is GET and url is regular (matching method name) and do not require HTTPS, just put an empty object
+  /*,
+  * example: {
+  *   type: 'POST', //(['GET']|'PUT'|'PATCH'|'POST'|'DELETE') [optional]
+  *   https: true, //(true|[false]) must call this api method via HTTPS [optional]
+  *   url: '/api/file/example?id={id}' //([/apiPrefix/apiPackageName/apiMethodName]|/<irregularApiMethodUrlOrWithParams>) [optional]
+  * }
+  *
+  * */
 });
