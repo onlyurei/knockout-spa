2af23dfe19216d84a8b7a5d2f1ceffcc07343ee2
Cheng Fan <cfan@groupon.com>
2016-03-08 10:31:23 -0600

    Make highlight a ko custom binding instead of using it directly in the file component; add helper util css classes.

--- js/component/file.js
+++ js/component/file.js
@@ -1,14 +1,10 @@
 define([
-  '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js', 'jsface', 'ko',
-  'text!../../template/component/file.html',
-  'css!//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css', 'css!../../css/component/file.css',
-  'jquery', 'sugar'
-], function (highlight, Class, ko, template) {
+  'jsface', 'ko', 'text!../../template/component/file.html', 'css!../../css/component/file.css', 'jquery', 'sugar'
+], function (Class, ko, template) {
 
   var File = Class({
     constructor: function (params) {
       this.content = ko.observable('');
-      this.id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
       this.getContent(params.url());
       params.url.subscribe(this.getContent.bind(this));
       this.affix = ko.computed(function () { return params.url().from(params.url().lastIndexOf('.')).remove('.') });
--- js/lib-ext/knockout-custom-bindings-lib.js
+++ js/lib-ext/knockout-custom-bindings-lib.js
@@ -11,7 +11,41 @@ define(['knockout', 'jquery'], function (ko) {
   };
   ko.bindingHandlers.showdown.update = ko.bindingHandlers.showdown.init;
 
-  /* TODO: add other third-party lib ko custom bindings */
+  ko.bindingHandlers.highlight = {
+    init: function (element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+      if (value) {
+        require([
+          '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js',
+          'css!//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css'
+        ], function (highlight) { //load lib on-demand to improve performance
+          $(element).find('code').each(function (i, block) {
+            highlight.highlightBlock(block);
+          });
+        });
+      }
+    }
+  };
+  ko.bindingHandlers.highlight.update = ko.bindingHandlers.highlight.init;
+
+  /* TODO: add other third-party lib ko custom bindings
+   * Tip: when to use a ko custom binding, and when to use a ko component?
+   *
+   * - If the actions are mostly DOM manipulations without the need of a formal data model, such as integrating with
+   * jQuery plugins, then it's more convenient, more modular, and lighter-weight to use ko custom bindings. Avoid writing
+   * nasty jQuery selectors at any cost - make it a ko custom binding instead, and use the exposed element to replace the
+   * selectors.
+   * http://knockoutjs.com/documentation/custom-bindings.html
+   *
+   * - If you are doing more than exposing the element to a DOM manipulation library, consider to use a full fledged ko
+   * component instead.
+   * http://knockoutjs.com/documentation/component-overview.html
+   *
+   * You can also use the combination of component and custom binding when appropriate.
+   * It's always about writing modular code and separating concerns.
+   * See the Files page provided in the boilerplate for reference.
+   *
+   * */
 
   return ko;
 
