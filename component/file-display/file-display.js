a579683a0b087b7426f31a4b6a0f6a2d548c67d2
Cheng Fan <fancheng33@hotmail.com>
2016-04-13 21:49:23 -0500

    Let <file-display> handle image displays.

--- component/file-display/file-display.js
+++ component/file-display/file-display.js
@@ -3,25 +3,45 @@ define([
 ], function (Class, ko, template) {
 
   var ViewModel = Class({
+    $static: {
+      INLINE_TAGS: {
+        img: { attr: 'src', affixes: ['gif', 'jpg', 'jpeg', 'png', 'tif', 'tiff'] }
+      }
+    },
     constructor: function (params) {
       this.content = '';
       if (ko.isObservable(params.url)) {
         params.url.subscribe(this.getContent.bind(this));
       }
+      this.inlineTag = '';
       ko.observe(this);
+      var url = ko.utils.unwrapObservable(params.url);
       var computed = {
         affix: function () {
-          var url = ko.utils.unwrapObservable(params.url);
           return url.from(url.lastIndexOf('.')).remove('.')
         }
       };
       Object.each(computed, function (key, value) {
         ko.defineComputedProperty(this, key, value);
       }.bind(this));
-      this.getContent(ko.utils.unwrapObservable(params.url));
+      this.getContent(url);
     },
     getContent: function (url) {
-      require(['text!' + url], this._content);
+      var parts = url.toLowerCase().split('.');
+      this.inlineTag = Object.find(ViewModel.INLINE_TAGS, function (key, value) {
+        return value.affixes.indexOf(parts[parts.length - 1]) >= 0;
+      });
+      if (this.inlineTag) {
+        this.content = '<{inlineTag} {attr}="{url}"></{inlineTag}>'.assign(
+          {
+            inlineTag: this.inlineTag,
+            attr: ViewModel.INLINE_TAGS[this.inlineTag].attr,
+            url: url
+          }
+        );
+      } else {
+        require(['text!' + url], this._content);
+      }
     }
   });
 
