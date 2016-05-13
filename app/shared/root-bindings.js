96a665d53530c576565f65da3c8de1e3ac0d71c5
Cheng Fan <cfan@groupon.com>
2016-05-13 23:28:54 +0800

    Add overridable tittle example in root-bindings.js.

--- app/shared/root-bindings.js
+++ app/shared/root-bindings.js
@@ -1,5 +1,5 @@
 define([
-  'app/shared/config', 'locale/resources', 'locale/strings', 'util/google-analytics', 'jquery', 'materialize',
+  'app/shared/config', 'locale/resources', 'locale/strings', 'util/google-analytics', 'jquery', 'sugar', 'materialize',
   'css!../../node_modules/materialize-css/dist/css/materialize.css', 'css!./shared.css'
   /* TODO: add other common dependencies and root bindings most pages need (this module is implicitly required by all pages).
    E.g.: swap Materialize UI to other CSS framework such as Bootstrap, Foundation, etc.
@@ -13,6 +13,10 @@ define([
     initExtra: function (pageName, pageData, pagePath, pageController) { // useful for common init tasks for all pages
       GA.trackPageView();
       /* TODO: modify to cater your app's needs (called on every page after the page's init handler) */
+    },
+    title: function () {
+      var title = this.page.data.title;
+      return title ? (Object.isFunction(title) ? title.call(this.page.data) : title) : this.page.name.titleize();
     }
   };
 
