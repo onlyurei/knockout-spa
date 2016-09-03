8fb0f4018a9464460932af4bdd432d7d858a95fc
Cheng Fan <cfan@groupon.com>
2016-09-03 08:20:22 -0500

    Add more comment to routes.js.

--- app/shared/routes.js
+++ app/shared/routes.js
@@ -1,7 +1,8 @@
 define({
   /* TODO: add all client side routes
    If using HTML5 history (real URLs instead of hashes),
-   remember to modify the server side 404 responder to return index.html  and change response code to 200. */
+   remember to modify the server side 404 responder to return index.html and change response code to 200.
+   Or, share the url path definition of these client-side routes to server side, to retain valid 404s.
   /* *
    *  Notes on routes definitions:
    *
@@ -13,7 +14,7 @@ define({
    *	see https://github.com/flatiron/director#routing-table for full documentation on route string definition
    *
    *  notes: 1. controller is optional but most likely needed unless there are no other internal links
-   *  		  (url change but page doesn't) on the page
+   *  		      (url change but page doesn't) on the page
    *         2. controller name should be the same as the part of the url after page module path;
    *            in the rare cases where page module definition has different value than the url path component then
    *            controller name should match the whole url
