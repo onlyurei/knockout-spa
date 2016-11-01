c5e46b394cd05df4c8d377082f5ff7fd5ca147dd
Cheng Fan <cfan@groupon.com>
2016-10-31 23:26:53 -0500

    Remove unnecessary quotes in common.js.

--- common.js
+++ common.js
@@ -1,23 +1,23 @@
 require.config({
   paths: {
     //lib
-    'cytoscape': 'node_modules/cytoscape/dist/cytoscape',
-    'director': 'node_modules/director/build/director',
-    'highlightjs': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min',
+    cytoscape: 'node_modules/cytoscape/dist/cytoscape',
+    director: 'node_modules/director/build/director',
+    highlightjs: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min',
     'highlightjs-css': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min',
-    'jquery': 'node_modules/jquery/dist/jquery',
-    'jsface': 'node_modules/jsface/dist/jsface',
-    'knockout': 'node_modules/knockout/build/output/knockout-latest',
+    jquery: 'node_modules/jquery/dist/jquery',
+    jsface: 'node_modules/jsface/dist/jsface',
+    knockout: 'node_modules/knockout/build/output/knockout-latest',
     'knockout-amd-helpers': 'node_modules/knockout-amd-helpers/build/knockout-amd-helpers',
     'knockout-es5-option4': 'node_modules/knockout-es5-option4/knockout-es5-option4',
-    'marked': 'node_modules/marked/lib/marked',
-    'materialize': 'node_modules/materialize-css/dist/js/materialize.min',
-    'i18n': 'lib/require-i18n',
-    'text': 'node_modules/requirejs-text/text',
-    'sugar': 'node_modules/sugar/sugar-full',
+    marked: 'node_modules/marked/lib/marked',
+    materialize: 'node_modules/materialize-css/dist/js/materialize.min',
+    i18n: 'lib/require-i18n',
+    text: 'node_modules/requirejs-text/text',
+    sugar: 'node_modules/sugar/sugar-full',
 
     //lib-ext
-    'ko': 'lib-ext/knockout-extended'
+    ko: 'lib-ext/knockout-extended'
   },
   packages: [
     {
@@ -27,10 +27,10 @@ require.config({
     }
   ],
   shim: {
-    'director': {
+    director: {
       exports: 'Router'
     },
-    'jsface': {
+    jsface: {
       exports: 'Class'
     }
   }
