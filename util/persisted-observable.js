73cb80c05cb81d0bf750a6d137283416d818a545
Cheng Fan <cfan@groupon.com>
2016-11-13 17:55:17 -0600

    Make storageKey in persisted-observable optional.

--- util/persisted-observable.js
+++ util/persisted-observable.js
@@ -3,22 +3,24 @@ define(['util/json', 'util/storage', 'jsface', 'ko', 'sugar'], function (Json, S
 
   var PersistedObservable = Class({
     constructor: function (storageKey, defaults, persist) {
-      var data = Object.merge(Object.isObject(defaults) ? defaults : {}, Storage.get(storageKey), true);
+      var data = Object.merge(Object.isObject(defaults) ? defaults : {}, (storageKey && Storage.get(storageKey)) || {}, true);
       Object.merge(this, data, true);
       ko.observe(this);
 
-      Json.traverse(this, function (key, value, object) {
-        var _key = '_' + key;
-        if (ko.isObservable(object[_key])) {
-          var subscription = object[_key].subscribe(persist || function () { //CAUTION: beware of memory leak, be sure to call dispose() when disposing
-              Storage.set(storageKey, this.serialize && this.serialize() || this);
-            }.bind(this));
-          Object.defineProperty(object, _key + '_subscription', {
-            value: subscription,
-            enumerable: false
-          })
-        }
-      }.bind(this));
+      if (storageKey) {
+        Json.traverse(this, function (key, value, object) {
+          var _key = '_' + key;
+          if (ko.isObservable(object[_key])) {
+            var subscription = object[_key].subscribe(persist || function () { //CAUTION: beware of memory leak, be sure to call dispose() when disposing
+                Storage.set(storageKey, this.serialize && this.serialize() || this);
+              }.bind(this));
+            Object.defineProperty(object, _key + '_subscription', {
+              value: subscription,
+              enumerable: false
+            })
+          }
+        }.bind(this));
+      }
 
       return this;
     },
